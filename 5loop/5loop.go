fe3fbec8581a8582c045f31f7adb2de4eabf9912
Avesh <aavesh88@gmail.com>
2023-02-07 22:43:28 +0530

    switch and loop

--- 5loop/5loop.go
+++ 5loop/5loop.go
@@ -3,24 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	var sum int = 0;
+	var sum int = 0
 	// no parentheses surrounding the three components of the for statement and the braces { } are always required.
-	for i := 0; i<10; i++ {
+	for i := 0; i < 10; i++ {
 		sum += i
 	}
 	// var declaration not allowed in for initializer
 	// for var i int = 0; i<10; i++ {
 	// 	sum += i
 	// }
-	
+
 	// The init and post statements are optional.
-	for ; sum < 50; {
+	for sum < 50 {
 		sum += sum
 	}
 	for sum < 100 {
 		sum += sum
 	}
-	for (sum < 100) {
+	for sum < 100 {
 		sum += sum
 	}
 	// Infinite loop
@@ -31,4 +31,8 @@ func main() {
 	// }
 
 	fmt.Println(sum)
+
+	for range "Hello" {
+		fmt.Println("Hello")
+	}
 }
--- 7switch/7switch.go
+++ 7switch/7switch.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"fmt";
-	"runtime";
+	"fmt"
+	"runtime"
 	"time"
 )
 
 func main() {
-	// break statement automatically provided after each case in Go. 
+	// break statement automatically provided after each case in Go.
 	// Go's switch cases need not be constants, and the values involved need not be integers.
 	switch os := runtime.GOOS; os {
 	case "darwin":
@@ -23,9 +23,9 @@ func main() {
 		fmt.Println("2 - 1")
 	case 2 - 2:
 		fmt.Println("2 - 2")
-	// Error if uncomment below lines
-	// case 3 - 2:
-	// 	fmt.Println("3 - 2")
+		// Error if uncomment below lines
+		// case 3 - 2:
+		// 	fmt.Println("3 - 2")
 	}
 
 	// Switch without a condition is the same as switch true.
@@ -39,5 +39,31 @@ func main() {
 		fmt.Println("Good afternoon.")
 	default:
 		fmt.Println("Good evening.")
+	}
+
+	// switch with multiple cases
+	t = time.Now()
+	d := t.Day()
+	switch d {
+	case 25, 7, 8:
+		fmt.Println("cleaning")
+	case 1, 2:
+		fmt.Println("Work")
+	default:
+		fmt.Println("Rest")
+	}
+
+	// The fallthrough keyword used to force the execution flow to fall through the successive case block.
+	d = 2
+	switch d {
+	case 1:
+		fmt.Println("Monday")
+	case 2:
+		fmt.Println("Tuesday")
+		fallthrough
+	case 3:
+		fmt.Println("Wednesday")
+	default:
+		fmt.Println("Rest")
 	}
 }
