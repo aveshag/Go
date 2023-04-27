4877c760aecaf99c4877c38be15d660b4355cd8b
Avesh <aavesh88@gmail.com>
2023-04-27 13:00:42 +0530

    slice

--- 12slice/12slice.go
+++ 12slice/12slice.go
@@ -7,6 +7,7 @@ import (
 )
 
 func main() {
+	// slice -> pointer (start location), length, capacity
 	primes := [6]int{2, 3, 5, 7, 11, 13}
 
 	chunk := primes[1:3]
@@ -25,7 +26,7 @@ func main() {
 	fmt.Println(chunk)
 
 	// []bool{true, true, false} is a slice literal
-
+	// declaration and initialization
 	chunk = []int{1, 2, 3, 4, 5, 6, 7}
 	// low <= high and high <= len(input)
 	fmt.Println(chunk)
@@ -34,16 +35,13 @@ func main() {
 	fmt.Println("chunk: ", chunk[len(chunk):len(chunk)])
 	// fmt.Println("chunk: ", chunk[len(chunk) + 1:]) // error
 
-	// The length of a slice is the number of elements it contains.
-	// The capacity of a slice is the number of elements in the underlying array, counting from the first element in the slice.
-
 	var s []int
 	fmt.Println(s, s[:], len(s), cap(s)) // s[0] error
 	if s == nil {
 		fmt.Println("nil!")
 	}
 
-	b := make([]int, 2, 5) // len(b)=2, cap(b)=5
+	b := make([]int, 2, 5) // len(b)=2, cap(b)=5 capacity is optional
 	fmt.Println(b)
 
 	// slice of slice
@@ -65,9 +63,13 @@ func main() {
 	}
 
 	fmt.Println(b, len(b), cap(b))
-	c := append(b, 1, 2, 3) // append(b, 1 , 2, 3, 4) check capacity of b
+	c := append(b, 1, 2, 3) // append(b, 1, 2, 3, 4) check capacity of b
+	d := append(b, 4, 5, 6)
+	e := append(b, 8, 9, 10, 11)
 	fmt.Println(b, len(b), cap(b))
-	fmt.Println(c, len(c), cap(c))
+	fmt.Println(c, len(c), cap(c)) // value of slice c also changed
+	fmt.Println(d, len(d), cap(d))
+	fmt.Println(e, len(e), cap(e)) // e doesn't change value of c and d as underlying array changed
 
 	// The range form of the for loop iterates over a slice or map.
 	// When ranging over a slice, two values are returned for each iteration. The first is the index, and the second is a copy of the element at that index.
@@ -86,4 +88,6 @@ func main() {
 	// Indices low and high work in the same way as with simple slice expressions. The only difference is max which sets result’s capacity to (max - low)
 
 	// y := []int is a error
+
+	// func copy(dst, src []type) int (must be of same datatype)
 }
